bd87fd4c78abb4765b60023ec7641b2025248f80
knmsyk <kounomasayuki@gmail.com>
2019-12-07 11:24:01 +0900

    Add UIKit extensions

---
+++ Sources/OptionalChainingKit/UIKit.swift
@@ -0,0 +1,14 @@
+//
+//  UIKit.swift
+//  OptionalChainingKit
+//
+//  Created by msyk on 2019/12/07.
+//
+
+import UIKit
+
+extension String {
+    public var uiImage: UIImage? {
+        UIImage(named: self)
+    }
+}
